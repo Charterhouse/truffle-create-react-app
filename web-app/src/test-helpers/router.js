87054ef2b5c6c8625ec74c37b1b4b50de8a3c3b0
Marcin Czenko <marcin@everydayproductive.com>
2017-12-13 13:27:30 +0100

    Added test helper  to support testing components that use react-router

---
+++ web-app/src/test-helpers/router.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { MemoryRouter as Router, Route } from 'react-router-dom'
+
+export const inRouter = (Component, path) => {
+  return (
+    <Router initialEntries={[ path ]} initialIndex={0}>
+      <Route exact path={path} component={Component} />
+    </Router>
+  )
+}
